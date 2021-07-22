11f5e80d5d143d2f9bc75e487b96715b1517ed0f
Jorge Corradi <jlcorradi@gmail.com>
2021-07-22 13:49:09 -0300

    Formatting

--- generate_receitas_txt.py
+++ generate_receitas_txt.py
@@ -7,41 +7,41 @@ import os
 today = datetime.now()
 
 parser = argparse.ArgumentParser(
-    description='Generate a set of contas_a_pagar records that can be later imported in dootax')
+        description='Generate a set of contas_a_pagar records that can be later imported in dootax')
 
 parser.add_argument('-o', '--output', dest='destination', default='/Users/jorgecorradi/Desktop',
-                    help='Where the file should be generated. Default: /Users/jorgecorradi/Desktop')
+        help='Where the file should be generated. Default: /Users/jorgecorradi/Desktop')
 parser.add_argument(
-    '-q', '--quantity', dest='quantity', help='Quantity of records to be generated', default=1, type=int)
+        '-q', '--quantity', dest='quantity', help='Quantity of records to be generated', default=1, type=int)
 parser.add_argument('-e', '--cod_empresa', dest='cod_empresa',
-                    help='Company code', required=True)
+        help='Company code', required=True)
 parser.add_argument('-s', '--cod_estab', dest='cod_estab', default='0001')
 parser.add_argument('-a', '--cod_arrecadacao',
-                    dest='cod_arrecadacao', default='0001', required=True)
+        dest='cod_arrecadacao', default='0001', required=True)
 parser.add_argument('-r', '--cod_receita', dest='cod_receita',
-                    help='Revenue code', required=True)
+        help='Revenue code', required=True)
 parser.add_argument('-d', '--det_receita', dest='det_receita',
-                    help='Revenue detail code')
+        help='Revenue detail code')
 parser.add_argument('--uf', dest='uf', help='UF favorecida', required=True)
 parser.add_argument('-b', '--vlr_base_calc', dest='vlr_base_calc',
-                    help='Valor da base de cálculo', default=0)
+        help='Valor da base de cálculo', default=0)
 parser.add_argument('-p', '--periodo', dest='periodo',
-                    required=False)
+        required=False)
 parser.add_argument('-v', '--data_vencimento', dest='data_vencimento', help='Due Date',
-                    default=today.strftime('%Y%m%d'))
+        default=today.strftime('%Y%m%d'))
 parser.add_argument('-g', '--data_pagamento', dest='data_pagamento', help='Due Date',
-                    default=today.strftime('%Y%m%d'))
+        default=today.strftime('%Y%m%d'))
 parser.add_argument(
-    '-c', '--console', help='If true, will show it instead of generating file', action='store_true')
+        '-c', '--console', help='If true, will show it instead of generating file', action='store_true')
 
 
 def get_first_day_of_last_month(date_as_string):
     d = datetime.strptime(date_as_string, '%Y%m%d')
     d.replace(
-        year=d.year if d.month > 1 else d.year - 1,
-        month=d.month - 1 if d.month > 1 else 12,
-        day=1
-    )
+            year=d.year if d.month > 1 else d.year - 1,
+            month=d.month - 1 if d.month > 1 else 12,
+            day=1
+            )
     return d.strftime('%Y%m%d')
 
 
@@ -65,16 +65,16 @@ if __name__ == '__main__':
         if args.vlr_base_calc != 0:
             conta['vlr_base_calc'] = args.vlr_base_calc
         conta['vlr_principal'] = 0 if args.vlr_base_calc != 0 else round(
-            random.randint(1000, 990000) / 100, 2)
+                random.randint(1000, 990000) / 100, 2)
         conta['periodo'] = args.periodo if args.periodo is not None else get_first_day_of_last_month(
-            args.data_vencimento)
+                args.data_vencimento)
         conta['num_doc_origem'] = num_doc_ini + i
         conta['info_complementar'] = f'Receita: {args.cod_arrecadacao} - {args.cod_receita} ({args.uf}). det: {args.det_receita if args.det_receita is not None else "-"}'
 
         contas_a_pagar.append(conta)
 
     result = json.dumps({'contas_a_pagar': contas_a_pagar},
-                        indent=2, separators=(',', ': '))
+            indent=2, separators=(',', ': '))
     if (args.console):
         print(result)
     else:
